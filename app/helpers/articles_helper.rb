e4e227f6b67b56c9ee3f1b44bbfca5b35b025089
Shawn <shawn.x.z@gmail.com>
2013-05-05 04:32:21 +0800

    fix to just display the bad words once

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,7 @@ module ArticlesHelper
   def article_result(article)
     content = "<p>#{article.filename}: #{article.good_word_percentage}%</p>"
     content += "<p class=mt10>生字集合:</p>"
-    content += "<p>#{article.bad_words.join("，")}</p>"
+    content += "<p>#{article.bad_words.uniq.join("，")}</p>"
     content
   end
 end
